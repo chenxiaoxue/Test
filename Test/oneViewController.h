9f1396561452b136b74c595b90c86a70f3ff0bde
xiaoxue <chenxiaoxue@xiaoxuedeiMac.local>
2017-10-25 16:43:04 +0800

    test new color set

---
+++ Test/oneViewController.h
@@ -0,0 +1,13 @@
+//
+//  oneViewController.h
+//  Test
+//
+//  Created by xiaoxue on 2017/10/25.
+//  Copyright © 2017年 xiaoxue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface oneViewController : UIViewController
+
+@end
---
+++ Test/oneViewController.m
@@ -0,0 +1,26 @@
+//
+//  oneViewController.m
+//  Test
+//
+//  Created by xiaoxue on 2017/10/25.
+//  Copyright © 2017年 xiaoxue. All rights reserved.
+//
+
+#import "oneViewController.h"
+
+@interface oneViewController ()
+
+@end
+
+@implementation oneViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+
+}
+
+
+
+
+@end
